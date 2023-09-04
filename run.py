a4ad1c4bfba92e8063eef5ffd38c937a40c659c5
Tes3103 <>
2023-09-04 18:50:53 +0000

    Add a while loop to reeat a user request until it returns valid

--- run.py
+++ run.py
@@ -15,17 +15,20 @@ SHEET = GSPREAD_CLIENT.open('sweet_cake_for_all1')
 
 def get_sales_data():
     """
-     the user input figures from the daily sales data
+     Get user input figures from the daily sales data
     """
-    print("please enter sales data from from the last market day.")
-    print("Data should be six figures, and separated by commas.")
-    print("Example:50,55,65,40,34,40\n")
+    while True:
+        print("please enter sales data from from the last market day.")
+        print("Data should be six figures, and separated by commas.")
+        print("Example:50,55,65,40,34,40\n")
 
-    data_str = input("Please enter your data here: ")
+        data_str = input("Please enter your data here: ")
     
-    sales_data = data_str.split(",")
-    print(sales_data)
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
 
 def validate_data(values):
@@ -35,12 +38,16 @@ def validate_data(values):
     or values are not exactly 6
     """
     try:
+        [int(value)for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"6 values are required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+        
+    return True
 
 
 get_sales_data()   
